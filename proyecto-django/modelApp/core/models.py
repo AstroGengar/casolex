0c5cec69ba7540020f7903a7064510b22c2510ce
Felipe Quiroz <felipe.i.quiroz.lara@gmail.com>
2021-06-15 13:28:36 -0400

    Agregar modelo de cliente y solicitud

--- proyecto-django/modelApp/core/models.py
+++ proyecto-django/modelApp/core/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
 # Create your models here.
+
+class Cliente(models.Model):
+    rut = models.IntegerField(max_length=9, null=False)
+    nombre = models.CharField(max_length=20, null=False)
+    apellido_paterno = models.CharField(max_length=20, null=False)
+    apellido_materno = models.CharField(max_length=20, null=False)
+    email = models.EmailField(max_length=254)
+    numero = models.PhoneNumberField()
+
+    def __str__(self):
+        return "%s %s" % (self.nombre, self.apellido_paterno, self.apellido_materno)
+
+class Solicitud(models.Model):
+    
+    CORPORATIVO = 'CP'
+    BANCA = 'BN'
+    DEFENSA = 'DF'
+    ADMINISTRATIVO = 'AD'
+    INMOBILIARIO = 'IM'
+    SEGUROS = 'SG'
+    MEDIOAMBIENTE = 'MA'
+
+
+    TIPO_SOLICITUD = [
+        (CORPORATIVO, 'Corporativo'),
+        (BANCA, 'Bancario'),
+        (DEFENSA, 'Defensa'),
+        (ADMINISTRATIVO, 'Administrativo'),
+        (INMOBILIARIO, 'Inmobiliario'),
+        (SEGUROS, 'Seguros'),
+        (MEDIOAMBIENTE, 'Medioambiente'),
+        
+
+    ]
+    
+    id = models.AutoField(primary_key=True)
+    rut = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    tipo = models.CharField(max_length=2, choices=TIPO_SOLICITUD, default=DEFENSA)
+
+    def __str__(self):
+        return self.id
+
+class FormaPago(models.Model):
+    pass
+
